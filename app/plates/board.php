c702dcd23241596fc74aa9fbf74d18a1bd2dc5ac
Rougin Gutib <rougingutib@gmail.com>
2024-11-03 17:42:06 +0800

    Fix issue in loading Board route

--- app/plates/board.php
+++ app/plates/board.php
@@ -1,7 +1,7 @@
 <?= $layout->load('main'); ?>
 
 <?= $block->body() ?>
-  <?= $plate->add('navbar', compact('url')) ?>
+  <?= $plate->add('navbar', compact('link')) ?>
 
   <div class="container">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusantium iste quas et, delectus debitis fugit minima temporibus illum. Architecto officia esse officiis, tempore, voluptates aliquam voluptatibus laudantium eius necessitatibus.</p>
