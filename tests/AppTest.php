5745d6b10a5e796abb8bd9e79b54b690b32eb5dc
Rougin Gutib <rougingutib@gmail.com>
2024-11-08 00:25:44 +0800

    Add unit tests for Express

---
+++ tests/AppTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Staticka\Expresso;
+
+/**
+ * @package Staticka
+ *
+ * @author Rougin Gutib <rougingutib@gmail.com>
+ */
+class AppTest extends Testcase
+{
+    /**
+     * @var \Staticka\Expresso\Express
+     */
+    protected $app;
+
+    /**
+     * @return void
+     */
+    public function doSetUp()
+    {
+        $path = __DIR__ . '/Fixture';
+
+        $app = new Express;
+
+        $app->setAppUrl('http://localhost:3977');
+        $app->setSiteUrl('http://localhost:3978');
+        $app->setRootPath($path);
+
+        $app->setBuildPath($path . '/build');
+        $app->setConfigPath($path . '/config');
+        $app->setPagesPath($path . '/pages');
+        $plates = __DIR__ . '/../app/plates';
+        $app->setPlatesPath($plates);
+        $app->setBuildPath($path . '/build');
+        $app->setTimezone('Asia/Manila');
+
+        $app->setFields(array());
+
+        $this->app = $app;
+    }
+
+    /**
+     * @return void
+     */
+    public function test_fixed_app_path()
+    {
+        /** @var string */
+        $expected = realpath(__DIR__ . '/../app');
+
+        /** @var string */
+        $actual = realpath(Express::getAppPath());
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_welcome_page()
+    {
+        $this->setRequest('GET', '/');
+
+        $this->app->run();
+
+        $this->expectOutputRegex('/Welcome to Expresso!/');
+    }
+
+    /**
+     * @param string $method
+     * @param string $uri
+     *
+     * @return void
+     */
+    protected function setRequest($method, $uri)
+    {
+        $_SERVER['REQUEST_METHOD'] = $method;
+        $_SERVER['REQUEST_URI'] = $uri;
+        $_SERVER['SERVER_NAME'] = 'localhost';
+        $_SERVER['SERVER_PORT'] = '8000';
+    }
+}
---
+++ tests/Testcase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Staticka\Expresso;
+
+use LegacyPHPUnit\TestCase as Legacy;
+
+/**
+ * @codeCoverageIgnore
+ *
+ * @package Staticka
+ *
+ * @author Rougin Gutib <rougingutib@gmail.com>
+ */
+class Testcase extends Legacy
+{
+    /** @phpstan-ignore-next-line */
+    public function setExpectedException($exception)
+    {
+        if (method_exists($this, 'expectException'))
+        {
+            $this->expectException($exception);
+
+            return;
+        }
+
+        /** @phpstan-ignore-next-line */
+        parent::setExpectedException($exception);
+    }
+}
