b12e301935702594d12dff83270a16e59148eb46
Rougin Gutib <rougingutib@gmail.com>
2026-06-07 08:59:21 +0800

    Remove unused method in "Testcase"

--- tests/Testcase.php
+++ tests/Testcase.php
@@ -13,20 +13,6 @@ use LegacyPHPUnit\TestCase as Legacy;
  */
 class Testcase extends Legacy
 {
-    /** @phpstan-ignore-next-line */
-    public function setExpectedException($exception)
-    {
-        if (method_exists($this, 'expectException'))
-        {
-            $this->expectException($exception);
-
-            return;
-        }
-
-        /** @phpstan-ignore-next-line */
-        parent::setExpectedException($exception);
-    }
-
     /**
      * @param string[] $fields
      *
